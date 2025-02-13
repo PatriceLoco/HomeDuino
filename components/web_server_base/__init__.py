56163e650e935285a185efa74b6ec9da81589cd8
PatriceLoco <patriceloco@gmail.com>
2025-02-13 19:07:51 +0100

    Update __init__.py

--- components/web_server_base/__init__.py
+++ components/web_server_base/__init__.py
@@ -38,3 +38,4 @@ async def to_code(config):
             cg.add_library("Update", None)
         # https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
         cg.add_library("esphome/ESPAsyncWebServer-esphome", "3.2.2")
+        cg.add_library("mobizt/FirebaseClient", "1.3.5")
