d5e01f1baeea1b307552a2cb7de3e695ce09789b
PatriceLoco <patriceloco@gmail.com>
2025-02-14 21:42:58 +0100

    Update md5.h

--- components/md5/md5.h
+++ components/md5/md5.h
@@ -4,12 +4,12 @@
 #ifdef USE_MD5
 
 #ifdef USE_ESP_IDF
-// #include "esp_rom_md5.h"
+#include "esp_rom_md5.h"
 #define MD5_CTX_TYPE md5_context_t
 #endif
 
 #if defined(USE_ARDUINO) && defined(USE_ESP32)
-#include "rom/md5_hash.h"
+// #include "rom/md5_hash.h"
 #define MD5_CTX_TYPE MD5Context
 #endif
 
