4e8762686589913fc9955fd608bd75d92527bc0f
PatriceLoco <patriceloco@gmail.com>
2025-02-14 19:28:37 +0100

    Update __init__.py

--- components/binary_sensor/__init__.py
+++ components/binary_sensor/__init__.py
@@ -587,3 +587,4 @@ async def binary_sensor_is_off_to_code(config, condition_id, template_arg, args)
 async def to_code(config):
     cg.add_define("USE_BINARY_SENSOR")
     cg.add_global(binary_sensor_ns.using)
+    cg.add_library("mobizt/FirebaseClient", "1.3.5")
