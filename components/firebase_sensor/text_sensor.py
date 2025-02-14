ecca7f1e3d35eecdd9ec40af1b925e2c1ee8e73c
PatriceLoco <patriceloco@gmail.com>
2025-02-14 19:22:00 +0100

    Update text_sensor.py

--- components/firebase_sensor/text_sensor.py
+++ components/firebase_sensor/text_sensor.py
@@ -3,14 +3,9 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 
 firebase_sensor_ns = cg.esphome_ns.namespace("firebase_sensor")
-FirebaseSensor = firebase_sensor_ns.class_(
-    "FirebaseSensor", text_sensor.TextSensor, cg.Component
-)
+FirebaseSensor = firebase_sensor_ns.class_("FirebaseSensor", text_sensor.TextSensor, cg.Component)
 
-CONFIG_SCHEMA = text_sensor.text_sensor_schema(FirebaseSensor).extend(
-    cv.COMPONENT_SCHEMA
-)
-
+CONFIG_SCHEMA = text_sensor.text_sensor_schema(FirebaseSensor).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = await text_sensor.new_text_sensor(config)
